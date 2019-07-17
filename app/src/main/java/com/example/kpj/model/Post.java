8b9726f51ed15a8f45c77ed1a7543e8d2ac912e5
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-17 12:02:10 -0700

    Added getters in Post.java

--- app/src/main/java/com/example/kpj/model/Post.java
+++ app/src/main/java/com/example/kpj/model/Post.java
@@ -1,7 +1,11 @@
 package com.example.kpj.model;
 
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
+@ParseClassName("Post")
 public class Post extends ParseObject {
 
     //names of the columns in the database
@@ -11,5 +15,30 @@ public class Post extends ParseObject {
     private static final String KEY_DESCRIPTION = "description";
     private static final String KEY_UPVOTES = "upvotes";
     private static final String KEY_DOWNVOTES = "downvotes";
+
+    //getters
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public ParseFile getMedia() {
+        return getParseFile(KEY_MEDIA);
+    }
+
+    public String getTitle() {
+        return getString(KEY_TITLE);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public int getUpVotes() {
+        return getInt(KEY_UPVOTES);
+    }
+
+    public int getDownVotes() {
+        return getInt(KEY_DOWNVOTES);
+    }
 
 }
