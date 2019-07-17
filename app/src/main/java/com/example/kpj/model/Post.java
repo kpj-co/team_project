cb326fa9732ddcb0adc57682ac68ee8ff9b94021
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-17 11:50:57 -0700

    Added constants to post

--- app/src/main/java/com/example/kpj/model/Post.java
+++ app/src/main/java/com/example/kpj/model/Post.java
@@ -1,4 +1,15 @@
 package com.example.kpj.model;
 
-public class Post {
+import com.parse.ParseObject;
+
+public class Post extends ParseObject {
+
+    //names of the columns in the database
+    private static final String KEY_USER = "user";
+    private static final String KEY_MEDIA = "media";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_UPVOTES = "upvotes";
+    private static final String KEY_DOWNVOTES = "downvotes";
+
 }
