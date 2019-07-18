359f411ff13b8c20add39cd1e034a024db4217f2
Kaiying Guo <48kaiying@gmail.com>
2019-07-17 17:11:22 -0700

    added xml for compose post and created compose post activity. Added search bar and compose button to course feed

---
+++ app/src/main/java/com/example/kpj/ComposePostActivity.java
@@ -0,0 +1,22 @@
+package com.example.kpj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComposePostActivity extends AppCompatActivity {
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_post);
+    }
+
+
+}
