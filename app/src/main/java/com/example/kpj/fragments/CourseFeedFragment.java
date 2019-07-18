16145527bcb4b0bf2a75ac337f766925dd3e03ee
Kaiying Guo <48kaiying@gmail.com>
2019-07-17 18:15:46 -0700

    saved edit text content of compose post activity as a Post and sent to parse

--- app/src/main/java/com/example/kpj/fragments/CourseFeedFragment.java
+++ app/src/main/java/com/example/kpj/fragments/CourseFeedFragment.java
@@ -1,5 +1,6 @@
 package com.example.kpj.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+
+import com.example.kpj.ComposePostActivity;
 import com.example.kpj.R;
 
 
@@ -45,11 +48,24 @@ public class CourseFeedFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_course_feed, container, false);
 
+        // Find views from xml
         rvCourseFeed = view.findViewById(R.id.rvCourseFeed);
         ibCompose = view.findViewById(R.id.ibCompose);
+        // TODO -- Figure out how to set up a search view
         //svSearch = view.findViewById(R.id.svSearch);
 
+        setComposeButtonListener();
         return view;
+    }
+
+    public void setComposeButtonListener() {
+        ibCompose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ComposePostActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
