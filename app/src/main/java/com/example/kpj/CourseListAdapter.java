968bcbaeb005d10e4fed19233200492a86b3d6ee
Jada Grant <jadagrant@fb.com>
2019-07-17 17:56:19 -0700

    Working on populating the course list view

--- app/src/main/java/com/example/kpj/CourseListAdapter.java
+++ app/src/main/java/com/example/kpj/CourseListAdapter.java
@@ -3,8 +3,10 @@ package com.example.kpj;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.kpj.model.Course;
 
@@ -15,31 +17,48 @@ public class CourseListAdapter extends RecyclerView.Adapter<CourseListAdapter.Vi
     private Context context;
     private List<Course> courses;
 
-    public CourseListAdapter(List<Course> courses){
+    public CourseListAdapter(Context context, List<Course> courses) {
+
         this.courses = courses;
+        this.context = context;
     }
 
 
     @NonNull
     @Override
     public CourseListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        context = viewGroup.getContext();
+        // inflate the layout
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // now create the view for course item
+        View courseView = inflater.inflate(R.layout.course_item, viewGroup, false);
+
+        // now return the new viewholder
+        return new ViewHolder(courseView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseListAdapter.ViewHolder viewHolder, int i) {
+        // this method is to bind the components of the layout to the user in parse
+        Course course = courses.get(i);
+        viewHolder.tvCourse.setText(course.getName());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return courses.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView tvCourse;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvCourse = itemView.findViewById(R.id.tvCourse);
         }
+
     }
 }
--- app/src/main/java/com/example/kpj/CourseListFragment.java
+++ app/src/main/java/com/example/kpj/CourseListFragment.java
@@ -1,17 +1,29 @@
 package com.example.kpj;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.kpj.model.Course;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseListFragment extends Fragment {
 
     private static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
+    private List<Course> courses;
+    private RecyclerView rvCourses;
+    private CourseListAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+
 
     public CourseListFragment() {
     }
@@ -30,16 +42,57 @@ public class CourseListFragment extends Fragment {
         if (getArguments() != null) {
             mPage = getArguments().getInt(ARG_PAGE);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course_list, container, false);
+        final View view = inflater.inflate(R.layout.fragment_course_list, container, false);
+
+        rvCourses = view.findViewById(R.id.rvCourse);
+        // creating the list
+        courses = new ArrayList<Course>();
+        // creating the adapter
+        adapter = new CourseListAdapter(getContext(), courses);
+        //set the layout manager
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        rvCourses.setLayoutManager(linearLayoutManager);
+        // set the adapter
+        rvCourses.setAdapter(adapter);
+
+        loadCourse();
+
+        return view;
     }
 
+    //    public void onViewCreated(View view, Bundle savedInstanceState) {
+//
+//
+//        // load the courses
+//        loadCourse(0);
+//    }
+//
+    private void loadCourse() {
+        final Course.Query courseQuery = new Course.Query();
 
-
-
+        courseQuery.findInBackground(new FindCallback<Course>() {
+            @Override
+            public void done(List<Course> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Course course = objects.get(i);
+                        courses.add(course);
+                        adapter.notifyItemInserted(courses.size() - 1);
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
+
+
+
