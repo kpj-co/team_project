f2f55be871ab3993949b024e38d41f46ecf44bfa
Kaiying Guo <48kaiying@gmail.com>
2019-07-17 17:29:05 -0700

    attached course feed xml to course feed fragment

--- app/src/main/java/com/example/kpj/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/kpj/fragments/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.kpj.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +10,10 @@ import android.view.ViewGroup;
 import com.example.kpj.R;
 
 public class ProfileFragment extends Fragment {
+
     private static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
+
 
     public ProfileFragment() {
     }
@@ -34,8 +37,13 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+
+
     }
 
 }
