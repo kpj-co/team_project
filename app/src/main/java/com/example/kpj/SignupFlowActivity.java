4003bddd0b60addc4f54b9ec4305b1aefea4b61c
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-19 16:02:58 -0700

    Starting fragment flow

---
+++ app/src/main/java/com/example/kpj/SignupFlowActivity.java
@@ -0,0 +1,16 @@
+package com.example.kpj;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupFlowActivity extends AppCompatActivity {
+
+    Fragment fragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_flow);
+    }
+
+}
--- app/src/main/java/com/example/kpj/fragments/UniversityFragment.java
+++ app/src/main/java/com/example/kpj/fragments/UniversityFragment.java
@@ -2,9 +2,14 @@ package com.example.kpj.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.kpj.R;
 import com.example.kpj.model.University;
@@ -15,6 +20,8 @@ public class UniversityFragment extends Fragment {
     private static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
     private ArrayList<University> universities;
+    private SearchView searchView;
+    private Button button;
     public UniversityFragment() {
     }
 
@@ -38,9 +45,29 @@ public class UniversityFragment extends Fragment {
 
         final View view = inflater.inflate(R.layout.fragment_university, container, false);
 
+        searchView = view.findViewById(R.id.svSearch);
+
+        /////TODO: Remove this button and its listener in the merge, i am just testing the fragment flow
+        button = (Button) view.findViewById(R.id.bTest);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSelectCoursesFragment();
+            }
+        });
         //init arraylist
         return view;
     }
 
-
+    public void goToSelectCoursesFragment() {
+        Fragment fragment = new SelectCoursesFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
 }
+
+
+
