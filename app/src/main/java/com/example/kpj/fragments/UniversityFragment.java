f8000d927c5833a3fec320fd256f758ee5d0b1d6
Jada Grant <jadagrant@fb.com>
2019-07-19 15:41:38 -0700

    Still working on University fragment, Profile now working

--- app/src/main/java/com/example/kpj/fragments/UniversityFragment.java
+++ app/src/main/java/com/example/kpj/fragments/UniversityFragment.java
@@ -2,19 +2,32 @@ package com.example.kpj.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kpj.R;
+import com.example.kpj.UniversityFragmentAdapter;
 import com.example.kpj.model.University;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UniversityFragment extends Fragment {
     private static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
-    private ArrayList<University> universities;
+    private ArrayList<University> filteredUniversities;
+    private RecyclerView recyclerView;
+    private UniversityFragmentAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private SearchView searchView;
+
     public UniversityFragment() {
     }
 
@@ -37,10 +50,32 @@ public class UniversityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         final View view = inflater.inflate(R.layout.fragment_university, container, false);
-
-        //init arraylist
+        //set up the searchview
+        searchView = (SearchView) view.findViewById(R.id.svSearch);
+        searchView.setQueryHint("University");
+        filteredUniversities = new ArrayList<>();
+        findUniversityByName();
+        recyclerView = view.findViewById(R.id.rvUniversity);
+        adapter = new UniversityFragmentAdapter(getContext(), filteredUniversities);
+        recyclerView.setAdapter(adapter);
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
         return view;
     }
 
-
+    private void findUniversityByName() {
+        final University.Query query = new University.Query();
+        query.findInBackground(new FindCallback<University>() {
+            @Override
+            public void done(List<University> objects, ParseException e) {
+                if(e == null){
+                    for(int i = 0; i < objects.size(); i++){
+                        University university =  objects.get(i);
+                        filteredUniversities.add(university);
+                        adapter.notifyItemInserted(filteredUniversities.size()-1);
+                    }
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/kpj/model/User.java
+++ app/src/main/java/com/example/kpj/model/User.java
@@ -1,17 +1,24 @@
 package com.example.kpj.model;
 
+import android.widget.SearchView;
+
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 @ParseClassName("_User")
 public class User extends ParseUser {
-
     public static final String KEY_PROFILE = "photoImage";
+    public static final String KEY_USERNAME = "username";
 
-    public ParseFile getProfileImage() { return getParseFile(KEY_PROFILE);  }
+    public String getUsername(){ return getString(KEY_USERNAME);}
 
-    public void setProfileImage(ParseFile photo) { put (KEY_PROFILE, photo);
-        ParseUser.getCurrentUser().saveInBackground();
+    public void setUsername(String username){ put(KEY_USERNAME, username); }
+
+    public static class Query extends ParseQuery<User>{
+
+        public Query() { super(User.class);}
+
     }
 }
