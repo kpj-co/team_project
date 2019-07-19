b56cef631196082ed68e93628fed501d87872ef8
Jada Grant <jadagrant@fb.com>
2019-07-19 12:16:33 -0700

    Added university

---
+++ app/src/main/java/com/example/kpj/PostAdapter.java
@@ -0,0 +1,20 @@
+package com.example.kpj;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.kpj.model.Post;
+
+import java.util.List;
+
+public class PostAdapter extends AppCompatActivity {
+
+    private Context context;
+    private List<Post> mPosts;
+
+    public PostAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        mPosts = posts;
+    }
+
+}
