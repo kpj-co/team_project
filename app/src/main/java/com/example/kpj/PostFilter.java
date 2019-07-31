686d9d9970df82c3c8e7939872eb84da123d3db5
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-31 16:58:48 -0700

    Filter by hashtags works. However the hashmaps will need to be stored locally because it takes a lot time giving an impression that it does not works

--- app/src/main/java/com/example/kpj/PostFilter.java
+++ app/src/main/java/com/example/kpj/PostFilter.java
@@ -20,8 +20,11 @@ public class PostFilter implements Filterable {
     private ArrayList<PostHashtagRelation> postHashtagRelations = new ArrayList<>();
     private ArrayList<Post> filteredPosts = new ArrayList<>();
 
-    //The int is how many hashtags has a certain post
-    private Map<Post, Integer> map = new HashMap<>();
+    //The int is how many hashtags has a certain post. The string is the post id
+    private Map<String, Integer> map = new HashMap<>();
+
+    //To retrieve posts by ID
+    private Map<String, Post> postByID = new HashMap<>();
 
     private PostAdapter adapter;
 
@@ -32,11 +35,14 @@ public class PostFilter implements Filterable {
         for(final Post post : posts) {
             PostHashtagRelation.Query query = new PostHashtagRelation.Query();
             query.whereEqualTo(PostHashtagRelation.KEY_POST, post);
+            query.withPost();
             query.findInBackground(new FindCallback<PostHashtagRelation>() {
                 @Override
                 public void done(List<PostHashtagRelation> objects, ParseException e) {
                     //Added to the map just when you find the relation, to avoid bugs in getFilter()
-                    map.put(post, 0);
+                    map.put(post.getObjectId(), 0);
+
+                    postByID.put(post.getObjectId(), post);
 
                     postHashtagRelations.addAll(objects);
 
@@ -56,15 +62,22 @@ public class PostFilter implements Filterable {
                 if(constraint != null && constraint.length() != 0) {
                     for(PostHashtagRelation postHashtagRelation : postHashtagRelations) {
                         if(constraints.contains(postHashtagRelation.getHashtag())) {
-                            Integer times = map.get((Post) postHashtagRelation.getPost());
+                            Post tempPost = (Post) postHashtagRelation.getPost();
+                            int times = map.get(tempPost.getObjectId());
                             times++;
+                            String id = tempPost.getObjectId();
+                            map.remove(tempPost.getObjectId());
+                            map.put(id, times);
+
+                            int i = 2;
                         }
                     }
 
                     for(Map.Entry element : map.entrySet()) {
                         //If the post has all the hashtags
                         if(element.getValue() == (Integer)constraints.size()) {
-                            filteredPosts.add((Post)element.getKey());
+                            String postID = (String)element.getKey();
+                            filteredPosts.add((Post)postByID.get(postID));
                         }
                     }
                     results.values = filteredPosts;
