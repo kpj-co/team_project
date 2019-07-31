3be2f332ff38b5f3b2f084512c06034de73b6c28
Kaiying Guo <48kaiying@gmail.com>
2019-07-30 17:59:17 -0700

    created PostImageRelation class and registered it as a subclass in the ParseApp file

---
+++ app/src/main/java/com/example/kpj/model/PostImageRelation.java
@@ -0,0 +1,33 @@
+package com.example.kpj.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+@ParseClassName("PostImageRelation")
+public class PostImageRelation extends ParseObject {
+
+    private static final String KEY_POST = "post";
+    private static final String KEY_IMAGE = "image";
+
+    public void setImage(ParseFile image) {
+        put(KEY_IMAGE, image);
+    }
+
+    public void setPost(Post post) {
+        put(KEY_POST, post);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public static class Query extends ParseQuery<PostImageRelation> {
+        public Query() {
+            super(PostImageRelation.class);
+        }
+    }
+
+
+}
