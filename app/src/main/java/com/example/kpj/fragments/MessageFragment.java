33c6ed39885528efa8f47332b8fd485b7d49543e
PabloMoreno2000 <pamta2000@gmail.com>
2019-08-06 14:41:32 -0700

    Remove whitespaces

--- app/src/main/java/com/example/kpj/fragments/MessageFragment.java
+++ app/src/main/java/com/example/kpj/fragments/MessageFragment.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import com.example.kpj.R;
 import com.example.kpj.RecyclerViewClickListener;
 import com.example.kpj.activities.ComposePostActivity;
@@ -32,9 +30,6 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.livequery.ParseLiveQueryClient;
 import com.parse.livequery.SubscriptionHandling;
-
-import org.parceler.Parcels;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,13 +48,9 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
     private LinearLayoutManager linearLayoutManager;
     //boolean that indicates if the liveQuery has been set
     private boolean isQueryLiveset = false;
-
     private List<Message> messages = new ArrayList<>();
-
     private MessageAdapter messageAdapter;
-
     private Course course;
-
     //TODO: Set this variable university dynamically
     private University university;
 
@@ -87,9 +78,7 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_message, container, false);
-
         currentUserUsername = ParseUser.getCurrentUser().getUsername();
-
         findViews(view);
         setListeners(view);
         //TODO: Remove this function when you can grab universities dynamically
@@ -97,7 +86,6 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
         prepareRecyclerView();
         setEndlessRecyclerViewScrollListener();
         populateRecyclerView(getCurrentCourseName(), true);
-
         return view;
     }
 
@@ -137,7 +125,6 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
                 if(!message.equals("")) {
                     //Send it to the database
                     pushMessageToDatabase(message);
-
                     //refresh messages
                     messageAdapter.notifyDataSetChanged();
                 }
@@ -147,41 +134,32 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
 
     void pushMessageToDatabase(String message) {
         Message newMessage = new Message();
-
         //set the message body of the current user
         newMessage.setDescription(message);
-
         //set the university of the current user
         if(university != null) {
             newMessage.setUniversity(university);
         }
-
         //Set the course of the user
         if(course != null) {
             newMessage.setCourse(course);
         }
-
         //Put the current user as the author of the message
         newMessage.setUser(ParseUser.getCurrentUser());
-
         //Clean the EditText
         etMessage.setText("");
-
         //Save the message in background
         newMessage.saveInBackground();
     }
 
     void populateRecyclerView(String courseName, final boolean scrollingDown) {
-
         //NOTE: THIS query will not be needed in the full version. We should know in which course are we
         final Course.Query courseQuery = new Course.Query();
-
         courseQuery.whereEqualTo("name", courseName);
         courseQuery.findInBackground(new FindCallback<Course>() {
             @Override
             public void done(List<Course> objects, ParseException e) {
                 course = objects.get(0);
-
                 final Message.Query messageQuery = new Message.Query();
                 messageQuery.setLimit(Message.MAX_NUMBER);
                 messageQuery.orderByDescending(Message.KEY_CREATED_AT);
@@ -207,19 +185,12 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
                                 messages.add(0, message);
                                 messageAdapter.notifyItemInserted(0);
                                 Log.d("MessageFragment", ": " +messages.size());
-//                                try {
-//                                    Log.d("MessageFragment", "Message:" + message.fetchIfNeeded().getString("description"));
-//                                } catch (ParseException e1) {
-//                                    e1.printStackTrace();
-//                                }
                             }
-
                             if(scrollingDown) {
                                 //scroll to the last message if you are not scrolling upwards
                                 recyclerView.scrollToPosition(messages.size() - 1);
                             }
                         }
-
                         if(!isQueryLiveset) {
                             //We need to set the ParseLiveQueryClient after finding the course
                             setParseLiveQueryClient();
@@ -233,7 +204,6 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
 
     //TODO: Remove this when you can set it dynamically
     void hardcodedFunction() {
-
         final University.Query universityQuery = new University.Query();
         universityQuery.whereEqualTo("name", "Facebook University");
         universityQuery.findInBackground(new FindCallback<University>() {
@@ -254,25 +224,17 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
 
     private void setParseLiveQueryClient() {
         ParseLiveQueryClient parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient();
-
-
         ParseQuery<Message> parseQuery = ParseQuery.getQuery(Message.class);
-
         parseQuery.whereEqualTo("course", course);
-
         SubscriptionHandling<Message> subscriptionHandling = parseLiveQueryClient.subscribe(parseQuery);
-
         subscriptionHandling.handleEvent(SubscriptionHandling.Event.CREATE, new
                 SubscriptionHandling.HandleEventCallback<Message>() {
                     @Override
                     //Add the element in the beginning of the recycler view and go to that position
                     public void onEvent(ParseQuery<Message> query, Message message) {
-
                         message.setUsername(currentUserUsername);
                         message.setParseFileUserImage(ParseUser.getCurrentUser().getParseFile("photoImage"));
-
                         ParseObject postParseObject = message.getPost();
-
                         if(postParseObject != null) {
                             message.setPostReference((Post) postParseObject);
                         }
@@ -288,14 +250,12 @@ public class MessageFragment extends Fragment implements RecyclerViewClickListen
                         });
                     }
                 });
-
         messageAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void recyclerViewListClicked(View v, int position) {
         Log.d("MessageFragment", "item position: " + position);
-
         //Move the content of the message to a post
         Intent intentPostMessage = new Intent(getContext(), ComposePostActivity.class);
         intentPostMessage.putExtra("message", messages.get(position));
